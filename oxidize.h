86bba42ef50535d85b42931335ec5babbecb2736
frakod <frakod@pm.me>
2021-10-21 17:06:03 +0530

    Create oxidize.h

---
+++ oxidize.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <cstdint>
+
+// Typedefs :-
+
+// needed for custom literals
+typedef unsigned long long ull;
+
+typedef std::int8_t  i8;
+typedef std::int16_t i16;
+typedef std::int32_t i32;
+typedef std::int64_t i64;
+
+typedef std::uint8_t  u8;
+typedef std::uint16_t u16;
+typedef std::uint32_t u32;
+typedef std::uint64_t u64;
+
+typedef float  f32;
+typedef double f64;
+
+typedef __int128_t  i128; // these are
+typedef __uint128_t u128; // not properly
+typedef long double f128; // supported yet
+
+// Custom Literals :-
+
+i8   operator ""i8  (ull n) {return static_cast<i8 >(n);}
+i16  operator ""i16 (ull n) {return static_cast<i16>(n);}
+i32  operator ""i32 (ull n) {return static_cast<i32>(n);}
+i64  operator ""i64 (ull n) {return static_cast<i64>(n);}
+
+u8   operator ""u8  (ull n) {return static_cast<u8 >(n);}
+u16  operator ""u16 (ull n) {return static_cast<u16>(n);}
+u32  operator ""u32 (ull n) {return static_cast<u32>(n);}
+u64  operator ""u64 (ull n) {return static_cast<u64>(n);}
+
+i128 operator ""i128(ull n) {return static_cast<i128>(n);}
+u128 operator ""u128(ull n) {return static_cast<u128>(n);}
+
+f32  operator ""f32 (ull n) {return static_cast<f32> (n);}
+f64  operator ""f64 (ull n) {return static_cast<f64> (n);}
+f128 operator ""f128(ull n) {return static_cast<f128>(n);}
+
+f32  operator ""f32 (f128 n){return static_cast<f32> (n);}
+f64  operator ""f64 (f128 n){return static_cast<f64> (n);}
+f128 operator ""f128(f128 n){return static_cast<f128>(n);}
