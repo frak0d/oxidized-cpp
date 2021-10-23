4931e0fa8665c813e0d0a57e79b9f14485b5f795
frakod <frakod@pm.me>
2021-10-23 18:50:21 +0530

    Added constevals

--- oxidize.h
+++ oxidize.h
@@ -25,23 +25,23 @@ typedef long double f128; // supported yet
 
 // Custom Literals :-
 
-i8   operator ""i8  (ull n) {return static_cast<i8 >(n);}
-i16  operator ""i16 (ull n) {return static_cast<i16>(n);}
-i32  operator ""i32 (ull n) {return static_cast<i32>(n);}
-i64  operator ""i64 (ull n) {return static_cast<i64>(n);}
+consteval i8   operator ""i8  (ull n) {return static_cast<i8 >(n);}
+consteval i16  operator ""i16 (ull n) {return static_cast<i16>(n);}
+consteval i32  operator ""i32 (ull n) {return static_cast<i32>(n);}
+consteval i64  operator ""i64 (ull n) {return static_cast<i64>(n);}
 
-u8   operator ""u8  (ull n) {return static_cast<u8 >(n);}
-u16  operator ""u16 (ull n) {return static_cast<u16>(n);}
-u32  operator ""u32 (ull n) {return static_cast<u32>(n);}
-u64  operator ""u64 (ull n) {return static_cast<u64>(n);}
+consteval u8   operator ""u8  (ull n) {return static_cast<u8 >(n);}
+consteval u16  operator ""u16 (ull n) {return static_cast<u16>(n);}
+consteval u32  operator ""u32 (ull n) {return static_cast<u32>(n);}
+consteval u64  operator ""u64 (ull n) {return static_cast<u64>(n);}
 
-i128 operator ""i128(ull n) {return static_cast<i128>(n);}
-u128 operator ""u128(ull n) {return static_cast<u128>(n);}
+consteval i128 operator ""i128(ull n) {return static_cast<i128>(n);}
+consteval u128 operator ""u128(ull n) {return static_cast<u128>(n);}
 
-f32  operator ""f32 (ull n) {return static_cast<f32> (n);}
-f64  operator ""f64 (ull n) {return static_cast<f64> (n);}
-f128 operator ""f128(ull n) {return static_cast<f128>(n);}
+consteval f32  operator ""f32 (ull n) {return static_cast<f32> (n);}
+consteval f64  operator ""f64 (ull n) {return static_cast<f64> (n);}
+consteval f128 operator ""f128(ull n) {return static_cast<f128>(n);}
 
-f32  operator ""f32 (f128 n){return static_cast<f32> (n);}
-f64  operator ""f64 (f128 n){return static_cast<f64> (n);}
-f128 operator ""f128(f128 n){return static_cast<f128>(n);}
+consteval f32  operator ""f32 (f128 n){return static_cast<f32> (n);}
+consteval f64  operator ""f64 (f128 n){return static_cast<f64> (n);}
+consteval f128 operator ""f128(f128 n){return static_cast<f128>(n);}
